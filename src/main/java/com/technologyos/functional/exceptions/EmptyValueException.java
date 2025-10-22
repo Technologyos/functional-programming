d3d6e20432bf0ff36953156011c85bcf193ed627
ArmandoSJ <just-jauregui@hotmail.com>
2025-10-21 23:48:33 -0600

    chore: se agrega getter en la exception de empty value exception

--- src/main/java/com/technologyos/functional/exceptions/EmptyValueException.java
+++ src/main/java/com/technologyos/functional/exceptions/EmptyValueException.java
@@ -1,7 +1,9 @@
 package com.technologyos.functional.exceptions;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @NoArgsConstructor
 public class EmptyValueException extends IllegalArgumentException {
    public EmptyValueException(String message){
