eb4d4bcaec2da3935b40f48c97989310c6f629d3
ArmandoSJ <just-jauregui@hotmail.com>
2026-01-04 21:10:30 -0600

    chore: se agrega espacio en la clase inicial

--- src/main/java/com/technologyos/functional/FunctionalApplication.java
+++ src/main/java/com/technologyos/functional/FunctionalApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FunctionalApplication {
+
    public static void main(String[] args) {
       SpringApplication.run(FunctionalApplication.class, args);
    }
